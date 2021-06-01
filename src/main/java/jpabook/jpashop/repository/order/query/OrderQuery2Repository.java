81607fed743f0cf8aed4bcfc6e382953fae2894d
jaewooDev <didwodn8822@gmail.com>
2021-06-01 17:32:31 +0900

    v6 commit

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQuery2Repository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQuery2Repository.java
@@ -69,4 +69,15 @@ public class OrderQuery2Repository {
         ).getResultList();
     }
 
+    public List< OrderFlatDto> findAllByDtos_flat() {
+        return em.createQuery(
+                "select new " +
+                        " jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name , o.orderDate , o.status , d.address , i.name, oi.orderPrice, oi.count)" +
+                        " from Order o"  +
+                        " join o.member m" +
+                        " join o.delivery" +
+                        " join o.orderItems oi" +
+                        " join oi.item i" , OrderFlatDto.class)
+                .getResultList();
+    }
 }
